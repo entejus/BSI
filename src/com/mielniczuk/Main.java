326fb6ebc755ad6adf4d9052e9614b95ed30902c
Jakub <mielniczuk.jakub@gmail.com>
2019-10-16 17:01:08 +0200

    Add simple GUI

--- src/com/mielniczuk/Main.java
+++ src/com/mielniczuk/Main.java
@@ -1,8 +1,35 @@
 package com.mielniczuk;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
 
+    private static JFrame frame;
+    private JTextArea textArea1;
+    private JTextArea textArea2;
+    private JTextArea textArea3;
+    private JButton startButton;
+    private JPanel mainJPanel;
+
+
+    public Main() {
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        frame = new JFrame("Cryptography");
+        frame.setContentPane(new Main().mainJPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(true);
     }
 }
