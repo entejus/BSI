072aa1b52ccc3dee1e85c6b12d8f597d8f193af9
Jakub <mielniczuk.jakub@gmail.com>
2019-10-16 18:05:33 +0200

    Decrypt text
    Added decryption of previously encrypted text

--- src/com/mielniczuk/Main.java
+++ src/com/mielniczuk/Main.java
@@ -13,6 +13,7 @@ public class Main {
     private KeyGenerator keygen;
     private SecretKey key;
     private Cipher cipher;
+    private byte[] encryptedData;
 
 
     private static JFrame frame;
@@ -25,44 +26,56 @@ public class Main {
     private void encrypt() throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
         FileInputStream inputFile = new FileInputStream("src/wolf.jpg");
         FileOutputStream outputFile = new FileOutputStream("src/wolfEncrypted.cfr");
+
+        String inputText = inputTextArea.getText();
+
         cipher = Cipher.getInstance("DESede");
         cipher.init(Cipher.ENCRYPT_MODE, key);
 
-        encryptText();
+        encryptText(inputText);
         encryptFile(inputFile, outputFile);
     }
 
     private void encryptFile(FileInputStream inputFile, FileOutputStream outputFile) throws IOException {
-        CipherOutputStream cipherOut = new CipherOutputStream(outputFile,cipher);
+        CipherOutputStream cipherOut = new CipherOutputStream(outputFile, cipher);
         byte[] buffer = new byte[2048];
         int readBytes;
-        while ((readBytes= inputFile.read(buffer)) != -1) {
+        while ((readBytes = inputFile.read(buffer)) != -1) {
             cipherOut.write(buffer, 0, readBytes);
         }
     }
 
-    private void encryptText() throws BadPaddingException, IllegalBlockSizeException {
-        String text = inputTextArea.getText();
-        String encryptedText = new String(cipher.doFinal(text.getBytes()));
+    private void encryptText(String inputText) throws BadPaddingException, IllegalBlockSizeException {
+        encryptedData = cipher.doFinal(inputText.getBytes());
+        String encryptedText = new String(encryptedData);
         encryptedTextArea.setText(encryptedText);
     }
 
-    private void decrypt() throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+
+    private void decrypt() throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
         FileInputStream inputFile = new FileInputStream("src/wolfEncrypted.cfr");
         FileOutputStream outputFile = new FileOutputStream("src/wolfDecrypted.jpg");
+
+        String encryptedText = encryptedTextArea.getText();
 
         cipher = Cipher.getInstance("DESede");
         cipher.init(Cipher.DECRYPT_MODE, key);
 
+        decryptText(encryptedText);
         decryptFile(inputFile, outputFile);
     }
 
     private void decryptFile(FileInputStream inputFile, FileOutputStream outputFile) throws IOException {
         byte[] buffer = new byte[2048];
         int readBytes;
-        while ((readBytes= inputFile.read(buffer)) != -1) {
-            outputFile.write(cipher.update(buffer,0,readBytes));
+        while ((readBytes = inputFile.read(buffer)) != -1) {
+            outputFile.write(cipher.update(buffer, 0, readBytes));
         }
+    }
+
+    private void decryptText(String encryptedText) throws BadPaddingException, IllegalBlockSizeException {
+        String decryptedText = new String(cipher.doFinal(encryptedData));
+        decryptedTextArea.setText(decryptedText);
     }
 
     private Main() throws NoSuchAlgorithmException {
@@ -79,7 +92,7 @@ public class Main {
         });
     }
 
-    public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
         frame = new JFrame("Cryptography");
         frame.setContentPane(new Main().mainJPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
